f37a1231582518630804b8003a644096060ac4c7
joaogabrieldasilva <joao.gabsilva1007@gmail.com>
2022-08-09 21:22:58 -0300

    operadores aritiméticos e funções

---
+++ script.js
@@ -0,0 +1,20 @@
+const x = 10;
+const y = 20;
+
+function somar() {
+  return x + y;
+}
+function subtrair() {
+  return x - y;
+}
+function dividir() {
+  return x / y;
+}
+function multiplicar() {
+  return x * y;
+}
+
+console.log(somar());
+console.log(subtrair());
+console.log(dividir());
+console.log(multiplicar());
